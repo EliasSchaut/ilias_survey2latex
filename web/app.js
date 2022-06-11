388c46d5061d02320659c6d4717867e3538ebf5f
EliasSchaut <eschaut@outlook.de>
2022-06-11 14:41:34 +0200

    add form post

--- web/app.js
+++ web/app.js
@@ -1,14 +1,29 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const multer = require('multer');
+const upload = multer();
 const path = require('path')
 const config = require("../config/config.json")
+const port = config.port
 const app = express()
 
-const port = config.port
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '\\wwwroot\\')))
+app.use(upload.array());
+
+
+// -----------------------------------
+// Routes
+// -----------------------------------
+app.post('/', function(req, res){
+    console.log("asd")
+    console.log(req.body);
+    res.send("received your request!");
+});
+// -----------------------------------
+
 
 // -----------------------------------
 // LISTEN
