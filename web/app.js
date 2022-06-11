4bf8a9adda208f20613631d82785126baf759e3d
EliasSchaut <eschaut@outlook.de>
2022-06-11 15:43:11 +0200

    save work

--- web/app.js
+++ web/app.js
@@ -1,26 +1,25 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const multer = require('multer');
-const upload = multer();
+const multer = require('multer')
 const path = require('path')
 const config = require("../config/config.json")
 const port = config.port
 const app = express()
 
+const upload = multer()
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, '\\wwwroot\\')))
-app.use(upload.array());
+app.use(upload.array())
 
 
 // -----------------------------------
 // Routes
 // -----------------------------------
-app.post('/', function(req, res){
-    console.log("asd")
-    console.log(req.body);
-    res.send("received your request!");
+app.post('/upload', upload.single("uploaded_file"), function(req, res) {
+    console.log(req.body)
+    res.send("OK")
 });
 // -----------------------------------
 
